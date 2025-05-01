f51969ef5932ed9191b493d538f8fede00fc8039
Zhaokun-Li <hjmh9196@leeds.ac.uk>
2025-05-01 13:33:28 +0000

    1

--- AbsoluteValue.asm
+++ AbsoluteValue.asm
@@ -1,36 +1,36 @@
 @R2
-M=0
+M=0        //initialize R2 to 0, indicating that the default R0 is not negative
 @R3
-M=0
+M=0        //initialize R3 to 0, indicating that the default R0 is not the minimum value
 @R0
-D=M
-@NEGATIVE
-D;JLT
+D=M        //load the value of R0 into the D register
+@NEGATIVE  
+D;JLT      //if R0<0, jump to the NEGATIVE label
 (POSITIVE)
 @R0
-D=M
+D=M        //load the value of R0 into D again
 @R1
-M=D
+M=D        //assign the value of R0 to R1
 @END
-0;JMP
+0;JMP      //jump to END and end the program
 (NEGATIVE)
 @R2
-M=1
+M=1        //set R2=1, indicating that the input value is negative
 @R0
-D=M
+D=M        //get negative values from R0
 @MIN_VALUE
-D;JEQ
+D;JEQ      //If R0==-32768, jump to the MIN_VALUE label
 D=!D
-D=D+1
+D=D+1      //convert negative numbers to positive, take the inverse and add one
 @R1
-M=D
+M=D        //write the result into R1, indicating that R1=- R0
 @END
-0;JMP
+0;JMP      //jump to END, program ends
 (MIN_VALUE)
 @R3
-M=1
+M=1        //set R3=1, mark input as minimum value
 @R1
-M=0
+M=0        //setting R1 to 0 means that its inverse cannot be calculated
 (END)
 @END
-0;JMP
+0;JMP      //program ends
--- Xor.asm
+++ Xor.asm
@@ -1,23 +1,23 @@
 @R0         
-D=M        
+D=M       //load the value from R0 into register D. At this point, D=R0 
 @R1     
-D=D|M      
+D=D|M     //operate OR operation on the values of D and R1. D=R0|R1 
 @R3         
-M=D    
+M=D       //write the result of the previous step into R3. R3=R0|R1
 @R0      
-D=M  
-@R1     
-D=D&M   
+D=M       //load the values from R0 into D again. D=R0。
+@R1       
+D=D&M     //operate AND operation on D and R1. D=R0&R1
 @R4     
-M=D       
+M=D       //write the result of the operation into R4. R4=R0&R1
 @R4   
-D=M 
-D=!D 
+D=M       //take out the value from R4 and put it into D. D=R4
+D=!D      //operate NOT operation on the values in D. D=~R4
 @R4   
-M=D    
+M=D       //save the reverse result back to R4. Now R4=~(R0&R1)
 @R3   
-D=M     
+D=M       //take out the value of R3 and place it in D
 @R4
-D=D&M     
+D=D&M     //operate AND operation between D and R4. D=(R0|R1)&~(R0&R1)
 @R2      
-M=D      
+M=D       //store the final result in R2
