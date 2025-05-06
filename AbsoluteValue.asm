e2d4381d79147cb5f300460a81452c3e808c8ac1
Zhaokun-Li <hjmh9196@leeds.ac.uk>
2025-05-06 15:41:16 +0000

    1

--- AbsoluteValue.asm
+++ AbsoluteValue.asm
@@ -4,8 +4,6 @@ M=0        //initialize R2 to 0, indicating that the default R0 is not negative
 M=0        //initialize R3 to 0, indicating that the default R0 is not the minimum value
 @R0
 D=M        //load the value of R0 into the D register
-//@R8
-//M=-32767
 @NEGATIVE  
 D;JLT      //if R0<0, jump to the NEGATIVE label
 (POSITIVE)
@@ -20,8 +18,6 @@ M=D        //assign the value of R0 to R1
 M=1        //set R2=1, indicating that the input value is negative
 @R0
 D=M        //get negative values from R0
-//@R8
-//D=D-M
 @MIN_VALUE
 D;JEQ      //If R0==-32768, jump to the MIN_VALUE label
 @R0
